8dff913371d8403da9986638fbddbee804513185
PraveenLokam <praveen1.lokam@gmail.com>
2022-03-12 15:48:34 -0600

    base changes

--- driver_job.py
+++ driver_job.py
@@ -1,4 +1,4 @@
-#from util.spark_util import SparkClient
+from util.spark_util import SparkClient
 import importlib.util
 import argparse
 
@@ -10,7 +10,7 @@ module_map = {
 class Driver:
     def __init__(self, source_file):
         self.source_file = source_file
-        #self.spark_client = SparkClient()
+        self.spark_client = SparkClient()
 
     def get_module(self):
         print(f"Fetching module {self.source_file.split('.')[0]}...")
