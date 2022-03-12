0a7fa281008c8b1cd27142f524b93a13bf6b8f95
PraveenLokam <praveen1.lokam@gmail.com>
2022-03-12 15:04:07 -0600

    base changes

--- driver_job.py
+++ driver_job.py
@@ -1,13 +1,14 @@
-from spark_util import SparkClient
+#from spark_util import SparkClient
 
 class Driver:
     def __init__(self):
-        self.spark_client = SparkClient()
+        pass
+        #self.spark_client = SparkClient()
 
     def execute_job(self):
         print('In execute job..')
         bucket = 'praveen-demo-bucket1'
         key = 'input/data.tsv'
-        df = self.spark_client.read_csv(bucket,key, delimiter='	')
+        #df = self.spark_client.read_csv(bucket,key, delimiter='	')
         print('Printing dataframe..')
-        df.show()
+        #df.show()
