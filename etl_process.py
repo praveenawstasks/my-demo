10b60b0decc8e5623098ba8c739c0c3f9c04de25
PraveenLokam <praveen1.lokam@gmail.com>
2022-03-16 20:21:29 -0600

    base changes

--- etl_process.py
+++ etl_process.py
@@ -3,6 +3,7 @@ import logging
 import sys
 import glob
 import shutil
+import boto3
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -59,16 +60,23 @@ class EtlProcess:
         logger.info(f"Writing dataframe into bucket : {self.source_bucket} and path : {temp_path}")
         self.spark_client.write_csv(df, self.source_bucket, temp_path, delimiter= delimiter, coalesce_count=1)
         self.spark_client.read_spark_temp_table(temp_table_name).show()
+        self.copy_file_to_path(temp_path)
 
-
+    def copy_file_to_path(self, source_path):
         logger.info("Writing as tab file for output...")
-        temp_s3_path = f"s3://{self.source_bucket}/{temp_path}"
-        logger.info(temp_s3_path)
-        print(glob.glob(temp_s3_path + "*.csv"))
-        temp_file_name = glob.glob(temp_s3_path + "*.csv")[0]
+        load_config = self.config['load']
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(self.source_bucket)
+        prefix_objs = bucket.objects.filter(Prefix=source_path)
+        csv_path = [obj.key for obj in prefix_objs if obj.key[-4:] == '.csv'][0]
+        logger.info(f"Generated temp csv : {csv_path}")
+        copy_source = {
+            'Bucket' : self.source_bucket,
+            'Key': csv_path
+        }
         output_path = f"{load_config['output_key_prefix']}{self.domain}"
         output_file_name = load_config['output_file_name']
         output_key = f"{output_path}/{output_file_name}"
-        output_file_path = shutil.copy2(temp_file_name, output_key)
-        logger.info(f'Written final output file at : {output_file_path}')
+        s3.meta.client.copy(copy_source, self.source_bucket, output_key)
+        logger.info(f'Written final output file in bucket: {self.source_bucket}  and key : {output_key}')
 
